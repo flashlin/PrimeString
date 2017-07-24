a0f16fba464da4197b8df4b372e214b8e743f49f
flash <flash.lin@gmail.com>
2017-07-24 13:15:52 +0800

    create new project

---
+++ PrimeString/PrimeString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeString
+{
+    public class PrimeString
+    {
+        public bool IsPrimeString(string text)
+        {
+            return false;
+        }
+    }
+}
---
+++ PrimeStringTests/PrimeStringTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PrimeStringTests
+{
+    [TestFixture]
+    public class PrimeStringTest
+    {
+        [Test]
+        [TestCase("abac", true)]
+        [TestCase("abab", false)]
+        [TestCase("aaaa", false)]
+        [TestCase("x", true)]
+        [TestCase("abc", true)]
+        [TestCase("fdsyffdsyffdsyffdsyffdsyf", false)]
+        [TestCase("utdutdtdutd", true)]
+        [TestCase("abba", true)]
+        [TestCase("ccccc", false)]
+        public void IsPrimeString(string inputValue, bool isPrimeString)
+        {
+            // Arrange
+            var primeStr = new PrimeString.PrimeString();
+
+            // Act
+            bool result = primeStr.IsPrimeString(inputValue);
+
+            // Assert
+            Assert.AreEqual(isPrimeString, result);
+        }
+    }
+}
